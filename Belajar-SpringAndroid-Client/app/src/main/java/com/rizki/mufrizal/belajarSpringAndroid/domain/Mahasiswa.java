452165b2b7074a5658e30560bad899971a2f32a4
Rizki Mufrizal <mufrizalrizki@gmail.com>
2015-04-29 07:40:52 +0700

    Add Domain Mahasiswa

---
+++ Belajar-SpringAndroid-Client/app/src/main/java/com/rizki/mufrizal/belajarSpringAndroid/domain/Mahasiswa.java
@@ -0,0 +1,34 @@
+package com.rizki.mufrizal.belajarSpringAndroid.domain;
+
+/**
+ * Created by rizki on 28/04/15.
+ */
+public class Mahasiswa {
+    private String npm;
+    private String nama;
+    private String kelas;
+
+    public String getNpm() {
+        return npm;
+    }
+
+    public void setNpm(String npm) {
+        this.npm = npm;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getKelas() {
+        return kelas;
+    }
+
+    public void setKelas(String kelas) {
+        this.kelas = kelas;
+    }
+}
